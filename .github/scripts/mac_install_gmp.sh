71cf2997a71a84037b8cc50b4aa46039c6ddf4f3
ryan-brott-skydio <110938665+ryan-brott-skydio@users.noreply.github.com>
2023-07-19 16:38:48 -0700

    Update mac_install_gmp.sh

--- .github/scripts/mac_install_gmp.sh
+++ .github/scripts/mac_install_gmp.sh
@@ -8,10 +8,12 @@
 set -ex
 
 if [ "$CIBW_ARCHS_MACOS" = "arm64" ]; then
+  export HOMEBREW_CURL_VERBOSE=1
+  export HOMEBREW_FORCE_BREWED_CURL=1
   mkdir arm-homebrew
   curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C arm-homebrew
-  HOMEBREW_CURL_VERBOSE=1 ./arm-homebrew/bin/brew install curl
-  response=$(HOMEBREW_FORCE_BREWED_CURL=1 ./arm-homebrew/bin/brew fetch --force --bottle-tag=arm64_big_sur gmp | grep "Downloaded to: " | cut -c 16-)
+  ./arm-homebrew/bin/brew install curl
+  response=$(./arm-homebrew/bin/brew fetch --force --bottle-tag=arm64_big_sur gmp | grep "Downloaded to: " | cut -c 16-)
   echo $response
   # parsed=($response)
   ./arm-homebrew/bin/brew install $response
